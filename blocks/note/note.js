7288c15f4006ce2594af79b5a36e97fa5cba98da
David Catalan <catalan@adobe.com>
2023-01-12 14:47:14 +0100

    optimize block code + adapt test

--- blocks/note/note.js
+++ blocks/note/note.js
@@ -10,24 +10,9 @@
  * governing permissions and limitations under the License.
  */
 
-import { readBlockConfig } from '../../scripts/scripts.js';
-
 export default function decorate(block) {
-  const cfg = readBlockConfig(block);
-  const $text = block.querySelector('div:first-of-type');
-  if ($text) {
-    $text.classList.add('content');
-
-    if (cfg.type) {
-      block.classList.add(cfg.type);
-    }
-    block.textContent = '';
-    /*
-      build container elements
-    */
-  
-    block.append($text);
-
+  if (block.firstElementChild) {
+    block.firstElementChild.classList.add('content');
     const icon = document.createElement('div');
     icon.classList.add('icon');
     block.append(icon);
