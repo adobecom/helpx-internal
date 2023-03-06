0ff65af5e6416707f091828b238025b3bd001986
Raghav Sharma <raghavs@adobe.com>
2023-03-06 14:02:29 +0530

    Changed the name of the test

--- test/blocks/procedure/procedure.test.js
+++ test/blocks/procedure/procedure.test.js
@@ -5,7 +5,7 @@ import { expect } from '@esm-bundle/chai';
 const { default: decorate } = await import('../../../blocks/procedure/procedure.js');
 
 describe('procedure', () => {
-  it('does the thing', async () => {
+  it('correctly decorates the procedure block', async () => {
     document.body.innerHTML = await readFile({ path: './mocks/procedure.html' });
     const procedure = document.querySelector('.procedure');
     expect(procedure).to.exist;
