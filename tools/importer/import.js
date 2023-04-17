c6bc428122523edabb8ca3574f6e06a8a9e38ee1
Raghav Sharma <raghavs@adobe.com>
2023-04-17 12:25:03 +0530

    Brought the note component to parity with the note component redesign on helpx

--- tools/importer/import.js
+++ tools/importer/import.js
@@ -70,7 +70,7 @@ export default {
 
     //    createColumnsFromDexterFlexContainers(main, document);
 
-    //   createBeforeAfterSliders(main, document);
+    createBeforeAfterSliders(main, document);
 
     //   createVideosEmbed(main, document);
 
