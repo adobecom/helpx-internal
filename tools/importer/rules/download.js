7e26891b74d158e86e123b60c0fabc381098776c
Raghav Sharma <raghavs@adobe.com>
2023-05-18 11:11:13 +0530

    Fix for download block import script

--- tools/importer/rules/download.js
+++ tools/importer/rules/download.js
@@ -6,8 +6,11 @@ const createDownload = (document) => {
       ['download'],
     ];
     download.querySelectorAll('.download-link-container').forEach((container) => {
+      const a = container.querySelector(':scope a');
+      a.href = `https://helpx-internal.corp.adobe.com${a.href}`;
+      console.info('poo', a.href);
       cells.push([
-        container.querySelector('a'),
+        a,
         container.querySelector('.download-description').textContent,
       ]);
     });
