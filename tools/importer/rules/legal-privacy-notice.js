d30bbbeff61214c465c0172fcf3b321c1ab1593c
Raghav Sharma <raghavs@adobe.com>
2023-05-31 15:18:15 +0530

    Small fix to the legal priovacy notice import script

--- tools/importer/rules/legal-privacy-notice.js
+++ tools/importer/rules/legal-privacy-notice.js
@@ -1,11 +1,11 @@
 const importLegalPrivacy = (document) => {
   // even though we use querySelectorAll, there should be no more than one per page
-  const notice = document.querySelector('div[daa-lh="HelpX in-article navigation|Legal & Privacy footer"]');
+  const notice = document.querySelector('.legalNotices');
   const a = document.createElement('a');
   a.href = 'https://main--helpx-internal--adobecom.hlx.page/content/help/en/fragments/legal-privacy-notice';
   a.textContent = 'https://main--helpx-internal--adobecom.hlx.page/content/help/en/fragments/legal-privacy-notice';
-  notice.insertAdjacentElement('beforebegin', a);
-  notice.remove();
+  notice?.insertAdjacentElement('beforebegin', a);
+  notice?.remove();
 };
 
 export default importLegalPrivacy;
