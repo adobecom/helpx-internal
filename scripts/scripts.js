9956896424e6a58a3c6a178e6bb70d3507e7d866
Raghav Sharma <raghavs@adobe.com>
2023-07-17 11:20:55 +0530

    Added also applies to

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -284,7 +284,11 @@ async function buildInternalBanner() {
     if (found) {
       const placeholders = await fetchPlaceholders();
       const dateFormat = new Date(parseInt(`${found.lastModified}000`, 10));
-      text.innerHTML = `${placeholders.lastUpdatedOn || 'Last updated on'} ${getMonthShortName((dateFormat.getMonth()))} ${dateFormat.getDate()}, ${dateFormat.getFullYear()}`;
+      const productNames = document.querySelector('meta[name="productnames"]')?.content.split(',');
+      const primary = document.querySelector('meta[name="primaryproductname"]')?.content;
+      const alsoAppliesTo = productNames.length ? ` | Also Applies to ${productNames.filter(x => x !== primary).join(', ')} ` : '';
+      text.innerHTML = 
+        `${placeholders.lastUpdatedOn || 'Last updated on'} ${getMonthShortName((dateFormat.getMonth()))} ${dateFormat.getDate()}, ${dateFormat.getFullYear()}${alsoAppliesTo}`;
     }
   }
 }
