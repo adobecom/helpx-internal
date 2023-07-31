21090599ef50e2483734b80907f535e82b9360b5
Raghav Sharma <raghavs@adobe.com>
2023-07-31 10:28:29 +0530

    Added button decoration (and added support for buttons inside <td>s); Also applies to now only appears if there are other thigns the article applies to; .page-title is now capable of being resized

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -130,6 +130,7 @@ const { loadArea, setConfig, loadStyle } = await import(`${miloLibs}/utils/utils
 function buildAutoBlocks() {
   try {
     fixTitle();
+    decorateButtons();
     decorateFirstH2();
     buildInternalBanner();
     fixTableHeaders();
@@ -280,6 +281,9 @@ async function buildInternalBanner() {
     banner.append(div);
     title.insertAdjacentElement('afterend', banner);
     decorateIcons(banner);
+    const setPaddingTop = () => { 
+      banner.style.paddingTop = `${title.offsetHeight + getHeaderMarginTop()}px`;
+    };
     banner.style.paddingTop = `${title.offsetHeight + getHeaderMarginTop()}px`;
     // needed to make sticky behaviour correct, specifically,
     // so that the internal banner is always below the sticky title
@@ -305,7 +309,8 @@ async function buildInternalBanner() {
 
     const productNames = document.querySelector('meta[name="productnames"]')?.content.split(',');
     const primary = document.querySelector('meta[name="primaryproductname"]')?.content;
-    const alsoAppliesTo = productNames?.length ? ` | Also Applies to ${productNames.filter(x => x !== primary).join(', ')} ` : '';
+    const productList = productNames?.length ? productNames.filter(x => x !== primary) : []
+    const alsoAppliesTo = productList.length ? ` | Also Applies to ${productList.join(', ')} ` : '';
     text.innerHTML = 
       `Last updated on ${getMonthShortName((dateFormat.getMonth()))} ${dateFormat.getDate()}, ${dateFormat.getFullYear()}${alsoAppliesTo}`;
   }
@@ -455,4 +460,35 @@ function getMonthShortName(monthNo) {
   const date = new Date();
   date.setMonth(monthNo);
   return date.toLocaleString('en-US', { month: 'short' });
+}
+
+/**
+ * decorates paragraphs containing a single link as buttons.
+ * @param {Element} element container element
+ */
+
+function decorateButtons(element=document.body) {
+  element.querySelectorAll('a').forEach((a) => {
+    a.title = a.title || a.textContent;
+    if (a.href !== a.textContent) {
+      const up = a.parentElement;
+      const twoup = a.parentElement.parentElement;
+      if (!a.querySelector('img')) {
+        if (up.childNodes.length === 1 && (up.tagName === 'P' || up.tagName === 'DIV' || up.tagName === 'TD')) {
+          a.className = 'button primary'; // default
+          up.classList.add('button-container');
+        }
+        if (up.childNodes.length === 1 && up.tagName === 'STRONG'
+            && twoup.childNodes.length === 1 && (twoup.tagName === 'P' || twoup.tagName === 'TD')) {
+          a.className = 'button primary';
+          twoup.classList.add('button-container');
+        }
+        if (up.childNodes.length === 1 && up.tagName === 'EM'
+            && twoup.childNodes.length === 1 && (twoup.tagName === 'P'||twoup.tagName === 'TD')) {
+          a.className = 'button secondary';
+          twoup.classList.add('button-container');
+        }
+      }
+    }
+  });
 }
