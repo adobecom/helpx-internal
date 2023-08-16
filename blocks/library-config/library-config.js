71f0b42270aa70f7fc38e1420ec25c475558e996
Raghav Sharma <raghavs@adobe.com>
2023-08-16 13:38:56 +0530

    A few small changes

--- blocks/library-config/library-config.js
+++ blocks/library-config/library-config.js
@@ -1,8 +1,6 @@
-
 export default async (block) => {
   const styles = document.querySelector('link[href="/styles/styles.css"]');;
   styles?.remove();
-  console.info(styles);
   const { default: init } = await import('https://milo.adobe.com/libs/blocks/library-config/library-config.js');
   init(block);
 }
